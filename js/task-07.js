0e3871f2ccbac8380dd0173c772260daaad4138a
Marina-Starikovskaja <marina.starikovskaja@gmail.com>
2022-12-06 15:54:44 +0200

    add start

--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const inputEl = document.querySelector('#font-size-control');
-const spanEl = document.querySelector('#text');
-
-inputEl.addEventListener('input', onInputChange);
-
-function onInputChange(event) {
-    console.log(event.currentTarget.value);
-    spanEl.style.fontSize = `${event.currentTarget.value}px`;
-}
